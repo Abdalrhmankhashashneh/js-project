9cafa6aaaf434fd718057bde97a8aa6bb8c46392
Abood kh <abdalrhmankhashasneh@gmail.com>
2022-04-04 12:52:16 +0300

    upload project

--- login.js
+++ login.js
@@ -1,63 +1,94 @@
-var rege = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/g;
-
-var em = "smsmspy@gmail.com"
-var pas = "qweytruio123*"
-var ale = document.getElementsByClassName("ale")
-function check() {
-
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
-
-    if (checkemail(email, em) && checkpass(password, pas)) {
-        ale[0].innerHTML = "ok "
-    }
-    else {
-        ale[0].innerHTML = "invalid email or password"
-    }
-    checkpass(password, pas)
+var id = localStorage.getItem("user_id")
+if (id) {
+    location.href = "resultpage/Result_page.html"
 }
+else {
+    var rege = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/g;
+    var x1 = JSON.parse(localStorage.getItem("users"))
+    var user_id = 0;
+    var ale = document.getElementsByClassName("ale")
+    function check() {
+
+        var email = document.getElementById('email').value;
+        var password = document.getElementById('password').value;
 
 
-function checkemail(email, em) {
+        var ob = obreturn(x1, email);
+        if (ob) {
+            var em = ob.email;
+            var pas = ob.password;
+            if (checkemail(email, em) && checkpass(password, pas)) {
+                localStorage.setItem("user_id", user_id)
+                if (ob.result == 0) {
+                    location.href = "quizpage/sasa.html"
+                }
+                else {
+                    location.href = "resultpage/Result_page.html"
+                }
+            }
+            else {
+                ale[0].innerHTML = "invalid email or password"
+            }
+        }
+        else {
+            ale[0].innerHTML = "invalid email or password"
 
-    if (email.match(rege)) {
-        if (email == em) {
-            return true
+        }
+
+    }
+
+    function obreturn(a, email) {
+        for (i = 0; i < a.userdata.length; i++) {
+            var check = a.userdata[i]
+            if (check.email == email) {
+                user_id = i;
+                return a.userdata[i]
+            }
+        }
+        return false;
+    }
+
+    function checkemail(email, em) {
+
+        if (email.match(rege)) {
+            if (email == em) {
+                return true
+            }
+            else {
+                return false
+            }
         }
         else {
             return false
         }
     }
-    else {
-        return false
-    }
-}
-function checkpass(password, pass) {
-    var regchar = /\w/
-    var regsp = /\d/
-    var op = true;
+    function checkpass(password, pass) {
+        var regchar = /\w/
+        var regsp = /\d/
+        var op = true;
 
-    if (!(password.match(regchar))) {
-        ale[1].innerHTML = "you have at least one charcter"
-        op = false;
-    } else {
-        ale[1].innerHTML = ""
+        if (!(password.match(regchar))) {
+            ale[1].innerHTML = "you have at least one charcter"
+            op = false;
+        } else {
+            ale[1].innerHTML = ""
+        }
+        if (!(password.match(regsp))) {
+            ale[2].innerHTML = "password must have at least 1 spisal character "
+            op = false
+        } else {
+            ale[2].innerHTML = ""
+        }
+        if (!(password.length >= 8)) {
+            ale[3].innerHTML = "password must have at least 8 character"
+            op = false;
+        }
+        else {
+            ale[3].innerHTML = ""
+        }
+        if (!(password == pass)) {
+            op = false;
+        }
+        return op;
     }
-    if (!(password.match(regsp))) {
-        ale[2].innerHTML = "password must have at least 1 spisal character "
-        op = false
-    } else {
-        ale[2].innerHTML = ""
-    }
-    if (!(password.length >= 8)) {
-        ale[3].innerHTML = "password must have at least 8 character"
-        op = false;
-    }
-    else {
-        ale[3].innerHTML = ""
-    }
-    if (!(password == pass)) {
-        op = false;
-    }
-    return op;
 }
---
+++ resultpage/Result_page.js
@@ -0,0 +1,49 @@
+
+var id = localStorage.getItem("user_id")
+var x1 = JSON.parse(localStorage.getItem("users"))
+var x2 = JSON.parse(localStorage.getItem("question"))
+var a = x1.userdata[id];
+var a2 = x2.q;
+var result = a.result;
+let f = document.getElementById("fail");
+let p = document.getElementById("pass");
+let btn = document.getElementById("ans_btn");
+let ans = document.getElementById("ans_table");
+document.getElementById("result").innerHTML = " Result : " + result
+document.getElementById("ca").innerHTML = " Correct Answers: " + result
+document.getElementById("wa").innerHTML = " Wrong Answers: " + (20 - result)
+
+if (result >= 10) {
+    p.style.display = "inline";
+}
+
+else {
+    f.style.display = "inline";
+}
+
+
+/***************************************************************** */
+var op = true;
+btn.onclick = function showAnswers() {
+    if (op) {
+        for (i = 0; i < a.answers.length; i++) {
+            var c = a2[i];
+            var c2 = a.answers;
+            ans.innerHTML += " <tr> " + " <td> " + c.question + " </td> " + " <td> " + c2[i] + " </td> " + " <td> " + c.correct + " </td> " + " </tr> "
+        }
+        ans.style.display = "table";
+        op = false
+    }
+}
+
+function home() {
+    var id = localStorage.getItem("user_id")
+    if (id) {
+        localStorage.removeItem("user_id")
+        location.href = "../login.html"
+    }
+    else {
+        location.href = "../welcomepage/HomePage/Index.html"
+    }
+}
+
